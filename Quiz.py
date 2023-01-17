4e5cfe8503d7db4b77d658944d3746e3ca0b439d
Oli <oliver.0t.taylor@gmail.com>
2023-01-17 16:21:05 +0000

    Added comments and improved readability

--- Quiz.py
+++ Quiz.py
@@ -1,39 +1,56 @@
 
 import random
 
-name = input("Enter your name: ")
 score=0
+
+name = input("Enter your name: ")
 
 def question(question,answer):
     global name
     global score
-    count = 0
+    count = 0 #Attempts number
+
     while count < 3:
+
         print(question,"\n \n")
-        userInput = (input("Enter the answer: ")).lower()
-        if userInput == answer:
+
+        if name.lower() == "cheatmode": #Secret hack
+            userInput = answer
+
+        else:
+            userInput = (input("Enter the answer: ")).lower() #Checks if input matches answer
+
+        if userInput == answer: #Executes if correct
             score+=1
             print(name," Correct \nScore: ",score)
             break
-        else:
+
+        else: #Executes if incorrect
             print(name," Incorrect \nScore: ", score)
             count+=1
 
 x=True
 while x == True:
-    choice = random.randint(1,4)
-    p1 = random.randint(1,1000)
+    
+    choice = random.randint(1,4) #Random number to decide whether to multiply or divide...
+    
+    p1 = random.randint(1,1000) # Random numbers to multiply together
     p2 = random.randint(1,1000)
+    
     if choice == 1:
         questionFuncInput = ("what is "+str(p1)+"*"+str(p2))
         answerInput = str(p1*p2)
+
     if choice == 2:
         questionFuncInput = ("what is "+str(p1)+"/"+str(p2))
         answerInput = str(p1/p2)
+
     if choice == 3:
         questionFuncInput = ("what is "+str(p1)+"+"+str(p2))
         answerInput = str(p1+p2)
+
     if choice == 4:
         questionFuncInput = ("what is "+str(p1)+"-"+str(p2))
         answerInput = str(p1-p2)
-    question(questionFuncInput,answerInput)
+
+    question(questionFuncInput,answerInput) #Calls function with question and answer
